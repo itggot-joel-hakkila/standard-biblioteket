b041f10bdfc04083476ed8786f0e4e8f8c0d826c
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-18 14:06:22 +0200

    max and min limit order does not matter

---
+++ dev/between.rb
@@ -0,0 +1,4 @@
+def between(num1, num2, num3)
+    return true if ((num1 >= num2) && (num1 <= num3)) || ((num1 <= num2) && (num1 >= num3))
+    return false
+end
--- lib/between.rb
+++
@@ -1,4 +0,0 @@
-def between(num1, num2, num3)
-    return true if (num1 >= num2) && (num1 <= num3)
-    return false
-end
