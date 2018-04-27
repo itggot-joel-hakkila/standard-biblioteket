f5696b7b7e3fd3a8245596f81d058a5dbe41fe37
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:54:45 +0200

    Added tomdok

--- lib/is_negative.rb
+++ lib/is_negative.rb
@@ -1,3 +1,13 @@
+# Public: Checks if a integer is negative
+# 
+# num - The Integer which is to be checked
+# 
+# Examples
+# 
+#   is_negative(-5)
+#   # => True
+# 
+# Returns the boolean value resulting from the function
 def is_negative(num)
     return false if num >= 0
     return true if num < 0
