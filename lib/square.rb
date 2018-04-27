5b29fcdb4cf3f6ec84f723f763b64755227d855b
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:56:16 +0200

    Added tomdok
    square

--- lib/square.rb
+++ lib/square.rb
@@ -1,3 +1,13 @@
+# Public: Squares an integer
+# 
+# num - The Integer which is to be squared
+# 
+# Examples
+# 
+#   square(3)
+#   # => 9
+# 
+# Returns the squared integer
 def square(num)
     return num**2
 end
