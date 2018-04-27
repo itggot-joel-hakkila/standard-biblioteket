1fa2bc4c610f56a3eaec60a140fef136685942fb
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:54:37 +0200

    Added tomdok

--- lib/cube.rb
+++ lib/cube.rb
@@ -1,3 +1,13 @@
+# Public: Cubes an integer
+# 
+# num - The Integer which is to be cubed
+# 
+# Examples
+#   
+#   cube(5)
+#   # => 125
+# 
+# Returns the integer value of the num cubed
 def cube(num)
-    return num**3
+    return num ** 3
 end
