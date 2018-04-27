27b7dee5e3fd1d58fde91b892ac606585e8f7892
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:55:13 +0200

    Added tomdok
    max of three

--- lib/max_of_three.rb
+++ lib/max_of_three.rb
@@ -1,3 +1,15 @@
+# Public: Checks which number out of three is the largest
+# 
+# num1 - The Integer value of the first number
+# num2 - The Integer value of the second number
+# num2 - The Integer value of the third number
+# 
+# Examples
+# 
+#   max_of_three(1,2,3)
+#   # => 3
+#
+# Returns the largest integer out of three
 def max_of_three(num1, num2, num3)
     largest = 0
     if num1 > num2
