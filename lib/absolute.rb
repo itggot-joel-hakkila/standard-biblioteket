9359a9d901a51d8eda1c84c1b16fd9d03a920b03
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:54:18 +0200

    Added tomdok

--- lib/absolute.rb
+++ lib/absolute.rb
@@ -1,3 +1,12 @@
+# Public: Returns the absolute value of an integer
+#
+# num - The Integer which the absolute value is derived from
+# Examples
+#
+#   absolute(-2)
+#   => 2
+#
+# Returns the absolute value
 def absolute(num)
     return num * -1 if num < 0
     return num if num > 0
