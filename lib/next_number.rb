77736647dc228f6ae022bca934d48b8c4ca7f386
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:56:04 +0200

    Added tomdok
    next number

--- lib/next_number.rb
+++ lib/next_number.rb
@@ -1,3 +1,13 @@
+# Public: Calculates the next number of an integer
+# 
+# num - The integer which is to derive the next number from
+# 
+# Examples
+# 
+#   next_number(5)
+#   # => 6
+# 
+# Returns the integer which is the next number derived from the function
 def next_number(num)
     return num + 1
 end
