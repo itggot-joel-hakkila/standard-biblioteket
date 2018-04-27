f3bfd2b6b96bb7430a46c7def4aec00bb5550a21
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:55:02 +0200

    Added tomdok
    max of four

--- lib/max_of_four.rb
+++ lib/max_of_four.rb
@@ -1,3 +1,16 @@
+# Public: Checks which number out of four is the largest
+# 
+# num1 - The Integer value of the first number
+# num2 - The Integer value of the second number
+# num2 - The Integer value of the third number
+# num3 - The Integer value of the four number
+# 
+# Examples
+# 
+#   max_of_four(1,2,3,4)
+#   # => 4
+#
+# Returns the largest integer out of four
 def max_of_four(num1, num2, num3, num4)
     largest = 0
     if num1 > num2
