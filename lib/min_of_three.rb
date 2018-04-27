30cf60f26e8df9e679323569000b95fe70c1a98e
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:55:49 +0200

    Added tomdok
    min of four

--- lib/min_of_three.rb
+++ lib/min_of_three.rb
@@ -1,3 +1,15 @@
+# Public: Checks which number out of three is the smallest
+# 
+# num1 - The Integer value of the first number
+# num2 - The Integer value of the second number
+# num2 - The Integer value of the third number
+# 
+# Examples
+# 
+#   min_of_three(1,2,3)
+#   # => 1
+#
+# Returns the smallest integer out of three
 def min_of_three(num1, num2, num3)
     smallest = 0
     if num1 < num2
