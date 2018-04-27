2f22909b3a310aab69bbdd0285e8bf80959f101d
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:55:56 +0200

    Added tomdok
    min of two

--- lib/min_of_two.rb
+++ lib/min_of_two.rb
@@ -1,3 +1,14 @@
+# Public: Checks which number out of two is the smallest
+# 
+# num1 - The Integer value of the first number
+# num2 - The Integer value of the second number
+# 
+# Examples
+# 
+#   min_of_three(1,2)
+#   # => 1
+#
+# Returns the smallest integer out of two
 def min_of_two(num1, num2)
     return num1 if num1 < num2
     return num2
