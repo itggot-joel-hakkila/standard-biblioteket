4146d781200da6587613a711a8548e8e40a43388
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:54:42 +0200

    Added tomdok

--- lib/is_even.rb
+++ lib/is_even.rb
@@ -1,3 +1,13 @@
+# Public: Checks if a integer is even
+# 
+# num - The Integer which is to be checked
+# 
+# Examples
+# 
+#   is_even(6)
+#   # => True
+# 
+# Returns the boolean value of the resulting comparison
 def is_even(num)
     return true if num % 2 == 0
     return false if num % 2 != 0
