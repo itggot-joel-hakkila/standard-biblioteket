99b437b1b034643b967627de67daa82d7ba0abf6
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:54:49 +0200

    Added tomdok

--- lib/is_odd.rb
+++ lib/is_odd.rb
@@ -1,3 +1,13 @@
+# Public: Checks if a integer is odd
+# 
+# num - The Integer which is to be checked
+# 
+# Examples
+# 
+#   is_odd(5)
+#   # => True
+# 
+# Returns the boolean value resulting from the function
 def is_odd(num)
     return true if num % 2 != 0
     return false if num % 2 == 0
