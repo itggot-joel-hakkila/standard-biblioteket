a425be2d37310319bb4baf8a32f08069ca39c969
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:55:44 +0200

    Added tomdok
    min of four

--- lib/min_of_four.rb
+++ lib/min_of_four.rb
@@ -1,3 +1,16 @@
+# Public: Checks which number out of four is the smallest
+# 
+# num1 - The Integer value of the first number
+# num2 - The Integer value of the second number
+# num2 - The Integer value of the third number
+# num3 - The Integer value of the four number
+# 
+# Examples
+# 
+#   min_of_four(1,2,3,4)
+#   # => 1
+#
+# Returns the smallest integer out of four
 def min_of_four(num1, num2, num3, num4)
     smallest = 0
     if num1 < num2
