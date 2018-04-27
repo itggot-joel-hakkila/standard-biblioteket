2483d3f54f1ad5bfc6834823fe82be57e96996ac
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:56:10 +0200

    Added tomdok
    previous number

--- lib/previous_number.rb
+++ lib/previous_number.rb
@@ -1,3 +1,13 @@
+# Public: Calculates the previous number of an integer
+# 
+# num - The integer which to derive the previous number from
+# 
+# Examples
+# 
+#   previous_number(5)
+#   # => 4
+# 
+# Returns the integer which is the previous number, derived from the function
 def previous_number(num)
     return num - 1
 end
