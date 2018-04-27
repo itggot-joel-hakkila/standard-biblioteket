de4cf713ef035bbdc7aac58d6c01964d7ca28058
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:56:22 +0200

    Added tomdok
    sum to

--- lib/sum_to.rb
+++ lib/sum_to.rb
@@ -1,3 +1,13 @@
+# Public: Sums every number up to the specified integer from and including 0
+# 
+# num - The number which is to be the upper limit and final number
+# 
+# Examples
+#
+#   sum_to(3)
+#   # => 6
+# 
+# Returns the sum up to the inputted number
 def sum_to(num)
     i = 0
     output = 0
