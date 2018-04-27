c553c838730f01b1ce8daf10c96dffb5f9c1b340
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:55:18 +0200

    Added tomdok
    max of two

--- lib/max_of_two.rb
+++ lib/max_of_two.rb
@@ -1,3 +1,14 @@
+# Public: Checks which number out of two is the largest
+# 
+# num1 - The Integer value of the first number
+# num2 - The Integer value of the second number
+# 
+# Examples
+# 
+#   max_of_two(1,2)
+#   # => 2
+#
+# Returns the largest integer out of two
 def max_of_two(num1, num2)
     return num1 if num1 > num2
     return num2
