9bb4dffce1a3deaf1654e084567fa80892d45468
itggot-joel-hakkila <joel.hakkila@itggot.se>
2018-04-27 08:54:25 +0200

    Added tomdok

--- lib/between_strict.rb
+++ lib/between_strict.rb
@@ -1,3 +1,18 @@
+# Public: Determines if a integer is between two limits while not being equal to either limit itself.
+# 
+# number - The Integer which is to be determined
+# limit1 - The Integer which is the first limit
+# limit2 - The Integer which is the be the second limit
+#
+# Examples
+# 
+#   between_strict(4, 1, 10)
+#   # => True
+#   
+#   between_strict(1, 1, 10)
+#   # => False
+#  
+# Returns the boolean value of the function
 def between_strict(number, limit1, limit2)
     return true if ((num1 > limit1) && (num1 < limit2)) || ((num1 < limit1) && (num1 > limit2))
     return false
